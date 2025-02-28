2fcdea8c16e5c7ba431800ea50ddb161ccba2648
D1nD1nh0x <d1nd1nh0x.69@gmail.com>
2025-02-28 11:14:02 -0300

    Implementação de operações básicas.

--- main.c
+++ main.c
@@ -1,8 +1,39 @@
 #include <stdio.h>
 
+// funções e procedimentos
+
+int soma (int n1, int n2);
+int subtracao (int n1, int n2);
+int multiplicacao (int n1, int n2);
+float divisao (int n1, int n2);
+
 int main () {
 	
 	
 	
 	return 0;
+}
+
+
+
+// funções e procedimentos
+
+
+int soma (int n1, int n2) {
+	return n1 + n2;
+}
+
+
+int subtracao (int n1, int n2){
+	return n1 - n2;
+}
+
+
+int multiplicacao (int n1, int n2){
+	return n1 * n2;
+}
+
+
+float divisao (int n1, int n2){
+	return (float) n1 / n2;
 }
