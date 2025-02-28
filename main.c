c81419b6ab9df0d1e7383eb9c95502df30741bab
D1nD1nh0x <d1nd1nh0x.69@gmail.com>
2025-02-28 11:39:18 -0300

    Implementação de loop para múltiplos calculos.

--- main.c
+++ main.c
@@ -7,8 +7,38 @@ int subtracao (int n1, int n2);
 int multiplicacao (int n1, int n2);
 float divisao (int n1, int n2);
 
+void mensagem_menu ();
+
+
 int main () {
 	
+	char op;
+	int numero1, numero2;
+	
+	
+	do {
+		
+		mensagem_menu();
+		printf("$ > "); scanf("%c", &op);
+		
+		while(getchar() != '\n'); // limpando o buffer do teclado
+		
+		switch (op) {
+			case 'm':
+				printf("Multiplos calculos\n");
+				break;
+			case 'u':
+				printf("um calculo\n");
+				break;
+			default:
+				printf("fim!\n");
+		}
+		
+		
+		if (op != 'm' && op != 'u')
+			break;
+	} while (1);
+
 	
 	
 	return 0;
@@ -36,4 +66,12 @@ int multiplicacao (int n1, int n2){
 
 float divisao (int n1, int n2){
 	return (float) n1 / n2;
+}
+
+
+void mensagem_menu () {
+	puts("Bem-vindo ao CalcMaster!");
+	putchar('\n');
+	puts("M - multiplos calculos\nU - para um calculo");
+	putchar('\n');
 }
